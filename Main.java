d3c51e3a6b4ba24f7ba94ff454be9cfe9f0f8dea
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-06 10:30:17 -0500

    created adding program

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int result = 0;
+        
+        System.out.println("Gimmi a number, any number!");
+        num = sc.nextInt();
+        
+        for(int i = 0; i <= num; i++)
+        {
+            result += i;
+        }
+        
+        System.out.printf("The sum of all numbers from 1 to %d is %d\n!", num, result);
 
     }
 }
